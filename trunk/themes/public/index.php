58a294d92d04cb2d1d7380d4cf8f40b8e173aaa0
Pedro Oliver <pedro@spworks.es>
2018-01-31 01:00:11 +0100

    New welcome page

--- trunk/themes/public/index.php
+++ trunk/themes/public/index.php
@@ -1,6 +1,3 @@
 <?
-include_once(_ROOT_."/externals/parsedown/Parsedown.php");
-$Parsedown = new Parsedown();
-
-echo $Parsedown->text(file_get_contents(_ROOT_."/../README.md")); # prints: <p>Hello <em>Parsedown</em>!</p>
+	$this->smarty->display("index.tpl");
 ?>
