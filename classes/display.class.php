4fa5ae97cc68cd3e36456d15e9de569d5b0420c1
Informatica <informatica@petardoscm.com>
2018-02-14 12:51:23 +0100

    Comments in Display Class

--- classes/display.class.php
+++ classes/display.class.php
@@ -1,27 +1,57 @@
-<?
+<?php
+/**
+ * This class acts as an example on where to position a DocBlock.
+ */
 class Display {
-    function __construct(){
-        $this->modules = new modules();
 
-    }
+    /**
+     * Init function to load Smarty
+     */
     function init(){
         $this->smarty = new SmartyCustom();
         $this->smarty->template_dir =  _THEMES_."/"._THEME_NAME_."/tpl";
     }
-    function p($value = '', $default = ''){
-        return isset($_POST[$value]) ? $_POST[$value] : $default;
+
+    /**
+     * Get POST value by key
+     *
+     * @param string $string Text string to search in POST key
+     * @param string $default Default value if key is not defined
+     *
+     * @return string
+     */
+    public function p($string = '', $default = ''){
+        return isset($_POST[$string]) ? $_POST[$string] : $default;
     }
-    function g($value = '', $default = ''){
+
+    /**
+     * Get GET value by key
+     *
+     * @param string $string Text string to search in GET key
+     * @param string $default Default value if key is not defined
+     *
+     * @return string
+     */
+    public function g($value = '', $default = ''){
         return isset($_GET[$value]) ? $_GET[$value] : $default;
     }
-    function startTheme(){
-		self::init();
+
+    /**
+     * Start theme operations
+     */
+    public function startTheme(){
+        self::init();
     }
-    function theme(){
-		$this->smarty->assign("base", _PATH_);
+
+    /**
+     * Load smarty base vars and start gui
+     */
+    public function theme(){
+        $this->smarty->assign("base", _PATH_);
         $this->smarty->assign("css", _TEMPLATES_."/"._THEME_NAME_."/css");
         $this->smarty->assign("js", _TEMPLATES_."/"._THEME_NAME_."/js");
         $this->smarty->assign("img", _TEMPLATES_."/"._THEME_NAME_."/img");
         include_once(_THEMES_."/"._THEME_NAME_."/index.php");
     }
 }
+?>
