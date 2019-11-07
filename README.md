5af658e295b7813b47bc350e730ba9599c53dd8e
Pedro Oliver <programacion@setecem.com>
2019-11-07 16:39:26 +0100

    Reworking ALL

--- README.md
+++
@@ -1,2 +0,0 @@
-# soanix/cavesman
-
---
+++ src/Cavesman/Cavesman.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cavesman;
+
+class Cavesman {
+    public function run($env = 'dev') {
+        exit("WORKS!");
+        /*require_once dirname(__FILE__) . '/Config/config.inc.php';
+        require_once _ROOT_ . '/install.php';
+        require_once _ROOT_ . '/controller.php';*/
+    }
+}
+?>
