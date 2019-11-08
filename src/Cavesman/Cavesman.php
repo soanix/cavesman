4f322dc7dd397b59669249d44f6f451790f2d7ae
SPWORKS\Pedro <soanix91@gmail.com>
2019-11-08 16:38:48 +0100

    Runs test

--- src/Cavesman/Cavesman.php
+++ src/Cavesman/Cavesman.php
@@ -3,11 +3,12 @@
 namespace Cavesman;
 
 class Cavesman {
-    public function run($env = 'dev') {
-        exit("WORKS!");
-        /*require_once dirname(__FILE__) . '/Config/config.inc.php';
-        require_once _ROOT_ . '/install.php';
-        require_once _ROOT_ . '/controller.php';*/
+
+    public static $env = 'dev';
+
+    public static function run($env = 'dev') {
+        self::$env = $env;
+        echo "RUNS ". self::$env;
     }
 }
 ?>
