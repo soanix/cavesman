b68f532e768d4175e3f3c81b09235580be3a08ec
SPWORKS\Pedro <soanix91@gmail.com>
2019-11-08 16:20:14 +0100

    Delete includes

--- src/Cavesman/Config/config.inc.php
+++ src/Cavesman/Config/config.inc.php
@@ -4,7 +4,6 @@ session_start();
 
 date_default_timezone_set('Europe/Madrid');
 
-require_once("defines.inc.php");
 if(_DEV_MODE_){
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
