062e151703b5c864a24ae6b0df722b30da5df8f9
Pedro Oliver <programacion@setecem.com>
2019-11-12 18:10:10 +0100

    Send as object config

--- src/Cavesman/Classes/Config.php
+++ src/Cavesman/Classes/Config.php
@@ -7,7 +7,7 @@ class Config {
     public static function get($config = NULL) {
         $file = _APP_."/config/" . $config  . ".json";
         if(file_exists($file)){
-            return json_decode(file_get_contents($file), true);
+            return json_decode(file_get_contents($file), false);
         }
         return null;
     }
