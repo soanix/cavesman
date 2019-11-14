fc50f808fd88e4c1c01726fe3dbfb1b900f5dea2
SPWORKS\Pedro <soanix91@gmail.com>
2019-11-14 16:24:42 +0100

    GIT version in template

--- src/Cavesman/Classes/Git.php
+++ src/Cavesman/Classes/Git.php
@@ -4,13 +4,8 @@ namespace Cavesman;
 
 class Git {
 
-  public static function version($params, $smarty) {
-    exec('git describe --always',$version_mini_hash);
-    exec('git rev-list HEAD | wc -l',$version_number);
-    exec('git log -1',$line);
-    $version['short'] = "v1.".trim($version_number[0]).".".$version_mini_hash[0];
-    $version['full'] = "v1.".trim($version_number[0]).".$version_mini_hash[0] (".str_replace('commit ','',$line[0]).")";
-    return $version;
-  }
+    public static function version($params, $smarty) {
+        return "0.1.0-dev";
+    }
 
 }
--- src/Cavesman/Classes/Smarty.php
+++ src/Cavesman/Classes/Smarty.php
@@ -37,7 +37,7 @@ class Smarty extends \Smarty {
 		$this->registerPlugin("function", "css", '\Cavesman\Smarty::smartyCss');
 		$this->registerPlugin("function", "img", '\Cavesman\Smarty::smartyImgUrl');
 		$this->registerPlugin("function", "js", '\Cavesman\Smarty::smartyJs');
-        $this->registerPlugin("function", "git_version", '\Cavesman\Smarty::version');
+        $this->registerPlugin("function", "git_version", '\Cavesman\Git::version');
 	}
 	public static function __install(){
 		if(!is_dir(_CACHE_."/views"))
