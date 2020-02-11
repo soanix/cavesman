a0a032ac72001517b5112e1b4e725a9de5aca6cf
SPWORKS\Pedro <soanix91@gmail.com>
2020-02-11 18:02:35 +0100

    git status

--- src/Cavesman/Classes/Config.php
+++ src/Cavesman/Classes/Config.php
@@ -9,6 +9,10 @@ class Config {
         if(file_exists($file)){
             return json_decode(file_get_contents($file), true);
         }
+        $file = _APP_."/config/" . $config  .".json";
+        if(file_exists($file)){
+            return json_decode(file_get_contents($file), true);
+        }
         return null;
     }
 
