ab1390c666bf6526877173561232a4fe6a58e261
SPWORKS\Pedro <soanix91@gmail.com>
2020-03-12 18:12:40 +0100

    FIX No funciona debug false si el server tiene errores habilitados

--- src/Cavesman/Config/config.inc.php
+++ src/Cavesman/Config/config.inc.php
@@ -4,9 +4,13 @@ session_start();
 
 date_default_timezone_set('Europe/Madrid');
 
-if(Cavesman\Config::get('params.debug')){
+if(Cavesman\Config::get('params.debug', true)){
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL | E_WARNING);
+}else{
+    ini_set('display_errors', 0);
+	ini_set('display_startup_errors', 0);
+	error_reporting(0);
 }
 ?>
