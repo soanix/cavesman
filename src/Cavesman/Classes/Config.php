6c182a4c21805b43a5ee0ae4f508032fcd305671
Pedro Oliver <programacion@setecem.com>
2020-10-27 17:10:13 +0000

    FIX config order env

--- src/Cavesman/Classes/Config.php
+++ src/Cavesman/Classes/Config.php
@@ -13,36 +13,31 @@ class Config {
             return $main['env'];
         return 'dev';
     }
-    public static function get($config = '') {
-        $params = explode(".", $config);
 
-        $file = _APP_."/config/" . $params[0]  .".". self::getEnv() .".json";
-        $array = [];
+    public static function get(string $config = '', $default = NULL){
+        $params = explode(".", $config);
+        $file = _APP_."/config/" . $params[0]  .".json";
+        $config = [];
         if(file_exists($file)){
-            $array =  json_decode(file_get_contents($file), true);
+            $config = json_decode(file_get_contents($file), true);
+        }
+
+        // Env config
+        $file = _APP_."/config/" . $params[0]  .".". self::getEnv() .".json";
+        if(file_exists($file)){
+            $config = array_replace_recursive($config, json_decode(file_get_contents($file), true));
+        }
+        if(file_exists($file)){
             foreach($params as $key =>  $param){
                 if($key){
-                    $array = $array[$param] ?? [];
-                    if(!$array)
-                        return self::getDefault($params);
+                    if(isset($config[$param]))
+                        $config = $config[$param];
+                    else
+                        return $default;
                 }
             }
         }
-        return $array;
-    }
-    private static function getDefault($params = []) {
-        $file = _APP_."/config/" . $params[0]  .".json";
-        $array = [];
-        if(file_exists($file)){
-            $array =  json_decode(file_get_contents($file), true);
-            foreach($params as $key => $param){
-                if($key){
-                    $array = $array[$param] ?? [];
-                    if(!$array)
-                        return [];
-                }
-            }
-        }
-        return $array;
+
+        return $config;
     }
 }
