08ed711fb2ac6b29cf35d90fff9e8818abf7715e
Pedro Oliver <programacion@setecem.com>
2021-02-23 12:13:22 +0100

    Fix menu

--- src/Cavesman/Classes/Menu.php
+++ src/Cavesman/Classes/Menu.php
@@ -2,47 +2,91 @@
 
 namespace Cavesman;
 
-class Menu {
+class Menu
+{
 
-    public static $items = array();
+    public static $items = [];
 
-    public static function addItem(array $item) : void {
-        if(isset($item['items']))
-            self::$items[] = $item;
-        else
-            foreach($item as $i)
-                self::$items[] = $i;
+    private static function isAssoc(array $arr)
+    {
+        if (array() === $arr) return false;
+        return array_keys($arr) !== range(0, count($arr) - 1);
     }
 
-    public static function render(array $params = array(), $smarty = null) : string {
+    public static function addItem(array $item): void
+    {
+        if (self::isAssoc($item)) {
+            $name = $item['name'] ?? "main";
+            if (!isset(self::$items[$name]))
+                self::$items[$name] = [
+                    "name" => "main",
+                    "order" => $item['order'],
+                    "items" => []
+                ];
+            if (isset($item['items']) && $item['items']) {
+                foreach ($item['items'] as $menu) {
+                    if (!isset(self::$items[$name]['items'][$menu['name']])) {
+                        self::$items[$name]['items'][$menu['name']] = $menu;
+                    } else {
+                        self::$items[$name]['items'][$menu['name']]['childs'] = array_merge_recursive(self::$items[$name]['items'][$menu['name']]['childs'], $menu['childs']);
+                    }
+                }
+            }
+        } else {
+            foreach($item as $itm){
+
+                $name = $itm['name'] ?? "main";
+                var_dump($name);
+                if (!isset(self::$items[$name]))
+                    self::$items[$name] = [
+                        "name" => "main",
+                        "order" => $itm['order'],
+                        "items" => []
+                    ];
+                if (isset($itm['items']) && $itm['items']) {
+                    foreach ($itm['items'] as $menu) {
+                        var_dump($menu['label']);
+                        if (!isset(self::$items[$name]['items'][$menu['name']])) {
+                            self::$items[$name]['items'][$menu['name']] = $menu;
+                        } else {
+                            self::$items[$name]['items'][$menu['name']]['childs'] = array_merge_recursive(self::$items[$name]['items'][$menu['name']]['childs'], $menu['childs']);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static function render(array $params = array(), $smarty = null): string
+    {
         $template_dir = Cavesman::$smarty->template_dir[0];
         $file = isset($params['file']) ? $params['file'] : false;
         $menu = isset($params['name']) ? $params['name'] : 'main';
-        if($file && file_exists($template_dir."/".$file)){
-            $file = $template_dir."/".$file;
-        }elseif(file_exists($file)){
+        if ($file && file_exists($template_dir . "/" . $file)) {
+            $file = $template_dir . "/" . $file;
+        } elseif (file_exists($file)) {
 
-        }elseif(file_exists($template_dir."/partial/menu/sidebar-item.tpl")){
-            $file = $template_dir."/partial/menu/sidebar-item.tpl";
+        } elseif (file_exists($template_dir . "/partial/menu/sidebar-item.tpl")) {
+            $file = $template_dir . "/partial/menu/sidebar-item.tpl";
         }
-        if(!file_exists($file))
-            throw new \Exception("TEMPLATE FILE NOT DEFINED OR DEFAULT TEMPLATE NOT FOUND (".$file.")");
-        usort(self::$items, function($a, $b) {
+        if (!file_exists($file))
+            throw new \Exception("TEMPLATE FILE NOT DEFINED OR DEFAULT TEMPLATE NOT FOUND (" . $file . ")");
+        usort(self::$items, function ($a, $b) {
             return $a['order'] <=> $b['order'];
         });
 
         $html = '';
 
-        foreach(self::$items as $item){
-            if(
+        foreach (self::$items as $item) {
+            if (
                 (!isset($item['menu']) && $menu == 'main') ||
                 (isset($item['menu']) && $item['menu'] == $menu)
-            ){
+            ) {
                 $binds = ["items" => $item];
 
-                foreach($item['items'] as $i){
-                    if(isset($i['binds'])){
-                        foreach($i['binds'] as $name => $value){
+                foreach ($item['items'] as $i) {
+                    if (isset($i['binds'])) {
+                        foreach ($i['binds'] as $name => $value) {
                             $binds[$name] = is_object($value) ? $value() : $value;
                         }
                     }
