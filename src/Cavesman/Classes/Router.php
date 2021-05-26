d38fdda20edafc45b01dfc38f42355db828dba2e
soanix <soanix91@gmail.com>
2021-05-26 13:09:50 +0200

    Update Router.php
    Migrate to soanix/router

--- src/Cavesman/Classes/Router.php
+++ src/Cavesman/Classes/Router.php
@@ -1,25 +1,9 @@
 <?php
 namespace Cavesman;
 
-use Bramus\Router\Router as Bramus;
 
-class Router extends Bramus{
+class Router extends \Soanix\Router\Router {
 
-    public static $instance;
-
-    function __construct(){ // Se conecta a mysqli
-
-    }
-
-
-    public static function Build()
-    {
-        if((self::$instance instanceof Bramus) === false)
-        {
-            self::$instance = new Bramus();
-        }
-        return self::$instance;
-    }
 }
 
 ?>
