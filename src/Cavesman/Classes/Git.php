627c9ac9722cd2b6cc501d0fa756db49d63962f2
Pedro Oliver <programacion@setecem.com>
2022-04-11 13:39:13 +0200

    Tags in git repo

--- src/Cavesman/Classes/Git.php
+++ src/Cavesman/Classes/Git.php
@@ -9,6 +9,18 @@ class Git
 {
     public static function version($params, $smarty)
     {
-       return substr(file_get_contents(dirname(__FILE__).'/../../../.git/refs/heads/master'),0,7);
+        $HEAD_hash = file_get_contents(dirname(__FILE__) . '/../../../.git/refs/heads/master'); // or branch x
+
+        $files = glob(dirname(__FILE__) . '/../../../.git/refs/tags/*');
+        foreach(array_reverse($files) as $file) {
+            $contents = trim(file_get_contents($file));
+
+            if($HEAD_hash === $contents)
+            {
+                exit('Current tag is ' . basename($file));
+            }
+        }
+
+        exit('No matching tag');
     }
 }
