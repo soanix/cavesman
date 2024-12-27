b8167479ddac90cf1027123ba488cc0d3aa6c024
SETECEM <101254179+setecem@users.noreply.github.com>
2024-12-27 12:25:50 +0100

    ADD generate function in Mail, now it's posible to generate new instance

--- src/Cavesman/Classes/Mail.php
+++ src/Cavesman/Classes/Mail.php
@@ -12,6 +12,27 @@ use PHPMailer\PHPMailer\SMTP;
  */
 class Mail
 {
+    public static $instance;
+
+    public static function generate() {
+        self::$instance = new PHPMailer(true);
+        self::$instance->CharSet = 'UTF-8';
+
+        //Server settings
+        if (Config::get("mail.debug"))
+            self::$instance->SMTPDebug = SMTP::DEBUG_SERVER;
+        self::$instance->isSMTP();
+        // Send using SMTP
+        self::$instance->Host = Config::get("mail.host");
+        self::$instance->SMTPAuth = Config::get("mail.smtp_auth");
+        self::$instance->Username = Config::get("mail.user");
+        self::$instance->Password = Config::get("mail.password");
+        self::$instance->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        self::$instance->Port = Config::get("mail.port");
+
+        //Recipients
+        self::$instance->setFrom(Config::get("mail.from.email"), Config::get("mail.from.name"));
+    }
 
     /**
      * @param string|array $to email@domain.tld or [{type: <to|cc|bcc>, email: string<email address>, name: string <Address name>}]
@@ -25,28 +46,16 @@ class Mail
     public static function send(string|array $to, string $subject = '', string|array  $body = '', array $files = [], array $embedImages = []): bool
     {
         // Instantiation and passing `true` enables exceptions
-        $mail = new PHPMailer(true);
-        $mail->CharSet = 'UTF-8';
+        if(!self::$instance instanceof self) {
+            self::generate();
+        }
 
-        //Server settings
-        if (Config::get("mail.debug"))
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-        $mail->isSMTP();
-        // Send using SMTP
-        $mail->Host = Config::get("mail.host");
-        $mail->SMTPAuth = Config::get("mail.smtp_auth");
-        $mail->Username = Config::get("mail.user");
-        $mail->Password = Config::get("mail.password");
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port = Config::get("mail.port");
 
-        //Recipients
-        $mail->setFrom(Config::get("mail.from.email"), Config::get("mail.from.name"));
 
         // Add address y $to is an email address
         if (is_string($to))
             if (filter_var($to, FILTER_VALIDATE_EMAIL))
-                $mail->addAddress($to);
+                self::$instance->addAddress($to);
             else
                 throw new Exception('Email address ' . $to . ' is not valid');
 
@@ -57,39 +66,39 @@ class Mail
                     throw new Exception('Email address ' . $address['email'] . ' is not valid');
 
                 if ($address['type'] == "to")
-                    $mail->addAddress($address['email'], $address['name']);
+                    self::$instance->addAddress($address['email'], $address['name']);
                 elseif ($address['type'] == "cc")
-                    $mail->addCC($address['email']);
+                    self::$instance->addCC($address['email']);
                 elseif ($address['type'] == "bcc")
-                    $mail->addBCC($address['email']);
+                    self::$instance->addBCC($address['email']);
             }
         }
 
 
         if (Config::get("mail.bbc.enable", false))
-            $mail->addBCC(Config::get("mail.reply.email"), Config::get("mail.reply.name"));
+            self::$instance->addBCC(Config::get("mail.reply.email"), Config::get("mail.reply.name"));
 
         if (Config::get("mail.reply.enable", false))
-            $mail->addReplyTo(Config::get("mail.reply.email"), Config::get("mail.reply.name"));
+            self::$instance->addReplyTo(Config::get("mail.reply.email"), Config::get("mail.reply.name"));
 
         // Content
 
-        $mail->Subject = $subject;
+        self::$instance->Subject = $subject;
 
-        $mail->isHTML();
+        self::$instance->isHTML();
         if(is_string($body))
-            $mail->msgHTML($body);
+            self::$instance->msgHTML($body);
         elseif(is_array($body)) {
-            $mail->Body = $body['html'];
-            $mail->AltBody = $mail->html2text($body['text']);
+            self::$instance->Body = $body['html'];
+            self::$instance->AltBody = self::$instance->html2text($body['text']);
         }
         foreach ($files as $file) {
-            $mail->addAttachment($file['file'], $file['name']);
+            self::$instance->addAttachment($file['file'], $file['name']);
         }
         foreach ($embedImages as $image) {
-            $mail->addEmbeddedImage($image['source'], $image['name']);
+            self::$instance->addEmbeddedImage($image['source'], $image['name']);
         }
-        return $mail->send();
+        return self::$instance->send();
     }
 
 }
