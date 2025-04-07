5d75e9815a91403c24b48a5d28bdb29711f8ed19
SETECEM <101254179+setecem@users.noreply.github.com>
2025-04-07 15:48:39 +0200

    Add entity interface to model db

--- src/Core/Db/Doctrine/Model/Model.php
+++ src/Core/Db/Doctrine/Model/Model.php
@@ -2,12 +2,13 @@
 
 namespace Cavesman\Db\Doctrine\Model;
 
+use Cavesman\Db\Doctrine\Interface\Entity;
 use Cavesman\Model\Base;
 
 /**
  * Used when model requires ID
  */
-abstract class Model extends Base
+abstract class Model extends Base implements Entity
 {
     public ?int $id = 0;
 }
