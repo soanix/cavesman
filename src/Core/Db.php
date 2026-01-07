84d5b151d34dbce9b564de58fab6d4ec439a100b
Pedro Oliver <setecem.programacion@gmail.com>
2026-01-07 16:09:38 +0100

    Auto generate proxys on config

--- src/Core/Db.php
+++ src/Core/Db.php
@@ -89,6 +89,18 @@ class Db
             isDevMode: Config::get($file . '.' . $server . '.dev_mode', true)
         );
 
+        $proxyDir = FileSystem::documentRoot() . '/var/Doctrine/Proxy';
+
+        if (!is_dir($proxyDir)) {
+            mkdir($proxyDir, 0777, true);
+        }
+
+        $config->setProxyDir($proxyDir);
+        $config->setProxyNamespace('DoctrineProxies');
+        $config->setAutoGenerateProxyClasses(
+            true
+        );
+
         if (Config::get('doctrine.support.native_lazy_object', false)) {
             $config->enableNativeLazyObjects(true);
         }
@@ -121,9 +133,6 @@ class Db
             self::$oConnection[$key]->getConfiguration()->addFilter('soft_delete', SoftDeleted::class);
             self::$oConnection[$key]->getFilters()->enable('soft_delete');
         }
-        if(!is_dir(FileSystem::documentRoot() . '/var/Doctrine/Proxy'))
-            mkdir(FileSystem::documentRoot() . '/var/Doctrine/Proxy', 0777, true);
-        self::$oConnection[$key]->getConfiguration()->setProxyDir(FileSystem::documentRoot() . '/var/Doctrine/Proxy');
 
         return self::$oConnection[$key];
     }
